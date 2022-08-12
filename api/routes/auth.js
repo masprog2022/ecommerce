0ec4d1a9b9bb0ddcd6b8fe0a6dc77d639c3ac005
Mauro Manuel <MAURO.MANUEL@UNITEL.CO.AO>
2022-08-12 11:41:49 +0100

    feat: route login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const CryptoJS = require('crypto-js')
+const CryptoJS = require('crypto-js');
+const { restart } = require('nodemon');
 
 //const User = require("../models/User")
 
@@ -12,7 +13,7 @@ router.post("/register", async (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
-        
+
     });
 
     try {
@@ -24,6 +25,40 @@ router.post("/register", async (req, res) => {
     }
 
 
+})
+
+
+// Login 
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username}) 
+
+        if(!user){
+            return res.status(401).json({ error: 'Wrong credentials!'})
+        }
+       // !user && res.status(401).send("Wrong credentials!")
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC)
+
+
+         const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+        if(Originalpassword !== req.body.password){
+            return res.status(401).json({ error: 'Wrong credentials!'})
+        }
+
+       // password !== req.body.password && res.status(401).send("Wrong credentials!")
+
+        const { password, ...others } = user._doc
+
+        return res.status(200).json(others)
+
+
+    }catch(err){
+      return res.status(500).json(err) 
+    }
+   
 })
 
 module.exports = router
