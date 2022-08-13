dba203e66cbdcf21f1e5a34819e64eca807b1151
Mauro Manuel <MAURO.MANUEL@UNITEL.CO.AO>
2022-08-13 19:25:33 +0100

    feat: route delete and get for id product

--- api/routes/product.js
+++ api/routes/product.js
@@ -10,33 +10,53 @@ const router = require('express').Router();
 
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
     const newProduct = new Product(req.body);
-  
+
     try {
-      const savedProduct = await newProduct.save();
-      res.status(200).json(savedProduct);
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  });
+});
 
 
-  // UPDATE 
+// UPDATE 
 
-  //UPDATE
+//UPDATE
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
     try {
-      const updatedProduct = await Product.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
-      res.status(200).json(updatedProduct);
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedProduct);
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  });
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted...");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET PRODUCT
+router.get("/find/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router
