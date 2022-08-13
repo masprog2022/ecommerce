46f4914e815d0e97c5b4ad5b8de0a7cf1e2ea042
Mauro Manuel <MAURO.MANUEL@UNITEL.CO.AO>
2022-08-13 19:21:00 +0100

    feat: route update product

--- api/routes/product.js
+++ api/routes/product.js
@@ -20,4 +20,23 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   });
 
 
+  // UPDATE 
+
+  //UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const updatedProduct = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedProduct);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
 module.exports = router
